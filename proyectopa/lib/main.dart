96b7560d4e571ad6526d23e223f1dac94766c1b0
Jossue <jossue2598@gmail.com>
2022-08-23 12:22:13 -0500

    sast/Pruebarecuperación

--- proyectopa/lib/main.dart
+++ proyectopa/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_app_check/firebase_app_check.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:proyectopa/screens/listado.dart';
@@ -9,6 +10,9 @@ import 'package:proyectopa/screens/listado.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  await FirebaseAppCheck.instance.activate(
+    webRecaptchaSiteKey: 'recaptcha-v3-site-key',
+  );
   runApp(MyApp());
 }
 
